6d7075cf7fec97e8c8764c7f71b578690e6ffa0a
reyessanchezo <90915894+reyessanchezo@users.noreply.github.com>
2024-12-04 15:15:59 -0500

    Update FinalProject.cpp

--- project/FinalProject/FinalProject.cpp
+++ project/FinalProject/FinalProject.cpp
@@ -7,7 +7,7 @@ Might include test for optimal path
 */
 #include <iostream>
 
-#include "Maze.cpp"
+#include "Maze.h"
 
 using namespace std;
 
@@ -20,9 +20,9 @@ int main()
 	const int rows = 5;
 	const int cols = 5;
 
-	/*Maze maze(rows,cols);
+	Maze maze(rows,cols);
 	maze.createMaze();
-	maze.displayMaze();*/
+	maze.displayMaze();
 	
 
 	cout << "All done !!\n\n\n\n"; // lots of space for my output window
