02f9d10e3a178eb1e5a670f29e6a198a2d529075
reyessanchezo <90915894+reyessanchezo@users.noreply.github.com>
2024-12-05 13:35:26 -0500

    Update QueueADT.h

--- project/FinalProject/QueueADT.h
+++ project/FinalProject/QueueADT.h
@@ -1,1 +1,20 @@
-#pragma once
+#ifndef QUEUEADT_H
+#define QUEUEADT_H
+
+#include <iostream>
+
+class QueueADT {
+public:
+    // Member functions that may change the queue
+    virtual bool Enqueue(int item) = 0;
+    virtual int Dequeue() = 0;
+
+    // Member functions that do not change the queue
+    virtual int GetLength() const = 0;
+    virtual bool IsEmpty() const = 0;
+    virtual int Peek() const = 0;
+    virtual void Print(std::ostream& printStream = std::cout,
+        const std::string& separator = ", ") const = 0;
+};
+
+#endif
