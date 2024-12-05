1f7fe3b6ca9e038e16288f5f9d9bc59607dc7bef
reyessanchezo <90915894+reyessanchezo@users.noreply.github.com>
2024-12-05 13:47:29 -0500

    Update Maze.h

--- project/FinalProject/Maze.h
+++ project/FinalProject/Maze.h
@@ -9,28 +9,24 @@ private:
     int cols;
 
 public:
-    // Constructor to initialize the maze with given dimensions
     Maze(int r, int c);
     Maze();
     Maze(const Maze* cp_maze);
 
-    // Destructor to free allocated memory
     ~Maze();
 
-    // Function to create a random maze
+    //  create a random maze
     void createMaze() const;
 
-    // Function to clear the maze
     void remakeMaze() const;
 
-    // Function to display the maze
     void displayMaze() const;
 
-    // Function to set a specific cell in the maze
     void setCell(int r, int c, int value) const;
 
-    // Function to get the value of a specific cell in the maze
     int getCell(int r, int c) const;
+
+	bool solveMazeBFS();
 };
 
 #endif // MAZE_H
