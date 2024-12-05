aeae8de175c59dfc2bd227b4f326ac617e027f91
reyessanchezo <90915894+reyessanchezo@users.noreply.github.com>
2024-12-05 13:53:06 -0500

    Update FinalProject.cpp

--- project/FinalProject/FinalProject.cpp
+++ project/FinalProject/FinalProject.cpp
@@ -16,17 +16,31 @@ using namespace std;
 int main()
 {
 	int square_size = 10;
+	int go = 1;
 
 	cout << "Testing...!\n" << "enter maze size (square): \n";
 
 	cin >> square_size;
-	Maze maze(square_size,square_size);
+	Maze maze(square_size, square_size);
 
 	maze.createMaze();
 	maze.displayMaze();
-	maze.remakeMaze();
-	maze.displayMaze();
-	
+
+	maze.solveMazeBFS();
+	cout << "new maze? 1 for yes, 0 for no(exit):";
+	cin >> go;
+
+	while (go == 1)
+	{
+		cout << "enter new maze size (square)";
+		cin >> square_size;
+		Maze new_maze(square_size, square_size);
+
+		maze.createMaze();
+		maze.displayMaze();
+		cout << "\n solving...\n";
+		maze.solveMazeBFS();
+	}
 
 	cout << "\nAll done !!\n\n\n\n"; // lots of space for my output window
 	return 0;
